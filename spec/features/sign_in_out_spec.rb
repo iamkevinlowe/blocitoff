564bb8c30addbe727d643e57594b9ce7339de991
Kevin Lowe <iamkevinlowe@gmail.com>
2015-06-30 14:00:28 -0700

    Fixed RSpec test

--- spec/features/sign_in_out_spec.rb
+++ spec/features/sign_in_out_spec.rb
@@ -4,48 +4,44 @@ require 'faker'
 describe "Sign in and out flow" do
   include TestFactories
 
+  before do
+    @user = authenticated_user
+    visit root_path
+
+    click_link 'Log In'
+    fill_in 'Email', with: @user.email
+    fill_in 'Password', with: @user.password
+
+    within 'form' do
+      click_button 'Log in'
+    end
+  end    
+
   describe "top navaigation" do
-    it "changes to indicate a user signed in and out" do
-      user = authenticated_user
-      visit root_path
+    it "changes to indicate a user signed in" do
+      expect(page).to have_content(@user.name)
+    end
 
-      click_link 'Log In'
-      fill_in 'Email', with: user.email
-      fill_in 'Password', with: user.password
-
-      within 'form' do
-        click_button 'Log in'
-      end
-
-      expect(page).to have_content(user.name)
-
+    it "changes to indicate a user signed out" do
       click_link 'log-out'
-
       expect(page).to have_content('Log In')
     end
   end
 
   describe "reset password" do
     it "changes user password" do
-      user = authenticated_user
-      visit root_path
-
-      click_link 'Log In'
-      fill_in 'Email', with: user.email
-      fill_in 'Password', with: user.password
-
-      within 'form' do
-        click_button 'Log in'
-      end
-
       click_link 'edit'
       new_password = Faker::Lorem.characters(10)
       fill_in 'Password', with: new_password
       fill_in 'Password confirmation', with: new_password
-      fill_in 'Current password', with: user.password
-
+      fill_in 'Current password', with: @user.password
       click_button 'Update'
-      expect(user.password).to eq(new_password)
+      click_link 'log-out'
+      click_link 'Log In'
+      fill_in 'Email', with: @user.email
+      fill_in 'Password', with: new_password
+      click_button 'Log in'
+      expect(page).to have_content('Signed in successfully.')
     end
   end
 end
