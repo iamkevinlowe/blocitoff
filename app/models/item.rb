b16987697d7bb276e94d356dcecae83cf757a9e0
Kevin Lowe <iamkevinlowe@gmail.com>
2015-06-30 22:47:20 -0700

    Updated views and added a method to delete old tasks within the application

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,8 @@ class Item < ActiveRecord::Base
   def days_left
     7 - (DateTime.now.utc.to_date - created_at.to_date).to_i
   end
+
+  def delete_if_older_than_7_days
+    delete if days_left < 0
+  end
 end
